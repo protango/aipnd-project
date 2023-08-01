cf9e5bbe8f212e56b56e1373d801ed26299f6da2
Zac Murray <zacharymurray@hotmail.com>
2023-08-01 10:09:43 +1000

    Add dirs

---
+++ predict.py
@@ -0,0 +1,4 @@
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='Description of your script.')
--- train.py
+++ train.py
@@ -1,4 +1,19 @@
 import argparse
+import json
+import torchvision.models as models
+import torchvision
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import importlib
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import os
 
 parser = argparse.ArgumentParser(
     prog='TrainModel', 
@@ -8,9 +23,15 @@ parser.add_argument('data_dir')
 parser.add_argument('--save_dir', default='.')
 parser.add_argument('--arch', default='vgg11')
 parser.add_argument('--learning_rate', type=float, default=0.01)
-parser.add_argument('--hidden_units', type=int, default=512)
+parser.add_argument('--hidden_units', type=int, default=4096)
 parser.add_argument('--epochs', type=int, default=10)
 parser.add_argument('--gpu', action='store_true')
 args = parser.parse_args()
 
-print(args)
+print(args.epochs)
+
+
+data_dir = 'flowers'
+train_dir = data_dir + '/train'
+valid_dir = data_dir + '/valid'
+test_dir = data_dir + '/test'
